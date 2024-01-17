322e12a3b195b661abd55328ec98bfb6b9657820
Romain Asnar <romain.asnar@gmail.com>
2024-01-17 19:02:51 +0700

    Add intro video.

--- web/src/payment-api/payment.api.ts
+++ web/src/payment-api/payment.api.ts
@@ -74,34 +74,6 @@ export class PaymentAPI {
       })),
     }
   }
-
-  // getOrderByPaymentIntentId(paymentIntentId: string): Promise<Checkout> {
-  //   console.log({ paymentIntentId })
-  //   return Promise.resolve({
-  //     email: 'romain.asnar@gmail.com',
-  //     date: new Date('2024-01-01'),
-  //     passId: 'party',
-  //     orderId: '42',
-  //     items: [
-  //       {
-  //         id: '1',
-  //         title: 'Party pass',
-  //         description: '',
-  //         amount: 2,
-  //         total: { amount: 23900, currency: 'EUR' },
-  //         includes: ['All parties in main venue', '3 welcome drinks'],
-  //       },
-  //       {
-  //         id: '2',
-  //         title: '1h traditional massage',
-  //         description: '',
-  //         amount: 2,
-  //         total: { amount: 2900, currency: 'EUR' },
-  //         includes: [],
-  //       },
-  //     ],
-  //   })
-  // }
 }
 
 export type Order = {
