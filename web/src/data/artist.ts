2924ff15f7b3b2a997c5b957c5da9390384e3f35
Romain Asnar <romain.asnar@gmail.com>
2024-01-22 02:30:01 +0700

    Update artists.

--- web/src/data/artist.ts
+++ web/src/data/artist.ts
@@ -1,50 +1,50 @@
 export const artists: { name: string; image: string }[] = [
   {
-    name: "Said 🇫🇷 & Oksana 🇺🇦",
-    image: "/images/artists/Said_Ok.jpg",
+    name: 'Said 🇫🇷 & Oksana 🇺🇦',
+    image: '/images/artists/Said_Ok.jpg',
   },
   {
-    name: "Rvan 🇫🇷 & Juliette 🇫🇷",
-    image: "/images/artists/Rvan.jpg",
+    name: 'Rvan 🇫🇷 & Juliette 🇫🇷',
+    image: '/images/artists/Rvan.jpg',
   },
   {
-    name: "Adrian 🇨🇴 & Carol 🇨🇳",
-    image: "/images/artists/Adrian_Carol.png",
+    name: 'Adrian 🇨🇴 & Carol 🇨🇳',
+    image: '/images/artists/Adrian_Carol.png',
   },
   {
-    name: "Heneco 🇫🇷",
-    image: "/images/artists/Heneco.jpg",
+    name: 'Heneco 🇫🇷',
+    image: '/images/artists/Heneco.jpg',
   },
   {
-    name: "Oncle Kani 🇫🇷",
-    image: "/images/artists/Oncle_Kani.jpg",
+    name: 'Oncle Kani 🇫🇷',
+    image: '/images/artists/Oncle_Kani.jpg',
   },
   {
-    name: "Quinn 🇨🇳",
-    image: "/images/artists/quinn.jpg",
+    name: 'Quinn 🇨🇳',
+    image: '/images/artists/quinn.jpg',
   },
   {
-    name: "Dave 🇫🇷",
-    image: "/images/artists/Dave.jpg",
+    name: 'Dave 🇬🇵',
+    image: '/images/artists/Dave.jpg',
   },
   {
-    name: "Stefanos 🇬🇧🇬🇷",
-    image: "/images/artists/stef.png",
+    name: 'Stefanos 🇬🇧🇬🇷',
+    image: '/images/artists/stef.png',
   },
   {
-    name: "Yoli 🇨🇳",
-    image: "/images/artists/Yoli.jpg",
+    name: 'Yoli 🇨🇳',
+    image: '/images/artists/Yoli.jpg',
   },
   {
-    name: "DJ Lenhy 🇫🇷",
-    image: "/images/artists/DJ_Lenhy.jpg",
+    name: 'DJ Lenhy 🇫🇷',
+    image: '/images/artists/DJ_Lenhy.jpg',
   },
   {
-    name: "DJ Selva 🇲🇾",
-    image: "/images/artists/selva.png",
+    name: 'DJ Selva 🇲🇾',
+    image: '/images/artists/selva.png',
   },
   {
-    name: "DJ Ploy 🇹🇭",
-    image: "/images/artists/ploy.png",
+    name: 'DJ Ploy 🇹🇭',
+    image: '/images/artists/ploy.png',
   },
-];
+]
